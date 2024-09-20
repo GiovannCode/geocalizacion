f8557e08eb7b593e8610bcaa1c35268cfe0ad218
Leonardo Nunez <leosternunez@gmail.com>
2024-09-19 19:07:31 -0600

    cambio estrucutra de tipos

---
+++ geo-direccion/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Geocalizacion</title>
+</head>
+
+<body>
+
+    <header class="header">
+        <div class="menu container">
+            <a href="" class="logo">FoodChi</a>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="login.php">Inicia sesion</a></li>
+                    <li><a href="registro.php">Crea tu cuenta</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="header-contenido container">
+            <h1>Pide comida rapida!</h1>
+            <p>Comida rapida a la comidad de tu hogar .</p>
+            <a href="#" class="btn-1">Pide ya!</a>
+        </div>
+    </header>
+    <section class="comida">
+        <div class="comida-contenido container">
+            <h2>Pide comida rapida al alcance de tu mano</h2>
+            <p class="txt-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, eos?</p>
+            <div class="comida-grupo">
+                <div class="comida-1">
+                    <img src="img/img2.jpg" alt="">
+                    <h3>Comida yupi yei</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, labore.</p>
+                </div>
+                <div class="comida-1">
+                    <img src="img/img3.jpg" alt="">
+                    <h3>Comida yupi yei</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, labore.</p>
+                </div>
+                <div class="comida-1">
+                    <img src="img/img4.jpg" alt="">
+                    <h3>Comida yupi yei</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, labore.</p>
+                </div>
+            </div>
+            <a href="" class="btn-1">Informacion</a>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ geo-direccion/login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+require 'db_conexion.php';
+if (isset($_POST['iniciar'])) {
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $select = $cnnPDO->prepare('SELECT * from user WHERE pass =? and email = ?');
+
+    $select->execute([$pass, $email]);
+    $count = $select->rowCount();
+    $campo = $select->fetch();
+
+    if ($count) {
+        $_SESSION['name'] = $campo['name'];
+        $_SESSION['email'] = $campo['email'];
+        $_SESSION['latitud'] = $campo['latitud'];
+        $_SESSION['longitud'] = $campo['longitud'];
+        header('location:sesion.php');
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="form-container">
+        <h2>Iniciar Sesión</h2>
+        <form method="post">
+            <label >Correo Electrónico:</label>
+            <input type="email"  name="email" placeholder="Escribe tu correo" required>
+
+            <label >Contraseña:</label>
+            <input type="password"  name="pass" placeholder="Escribe tu contraseña" required>
+
+            <input type="submit" name="iniciar">
+        </form>
+    </div>
+</body>
+</html>
